84cd4fae146d6e2c676cf4018a55b497807c83d5
Loïc Marchand <nilpa018@yahoo.fr>
2024-04-05 00:23:04 +0200

    chore(main): deleting useless function into utils/functions file

--- utils/functions.js
+++ utils/functions.js
@@ -4,11 +4,6 @@ export default function convertingDates(date) {
   return `${splittedDate[1]}/${splittedDate[0]}/${splittedDate[2]}`;
 }
 
-export function timeHour(date) {
-  const timeZone = `${date.getHours()}:${date.getMinutes()}`;
-  return timeZone;
-}
-
 export function returnTime(date) {
   const period = date.getHours() >= 0 && date.getHours() < 13 ? "am" : "pm";
   return period;
